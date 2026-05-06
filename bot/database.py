4c794f003f9d89b368debab596ca7c304430a0e4
Muhammadyusuf <muhammadyusu1199@gmail.com>
2026-05-06 16:25:42 +0300

    Update database.py

--- bot/database.py
+++ bot/database.py
@@ -7,7 +7,7 @@ import hashlib
 import os
 import sys
 from datetime import date, datetime
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 try:
     from config import DB_NAME
